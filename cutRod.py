40b2ab37772c70173a19ddcd49cf6eb08519c99e
Shantanu <shantanuanand@live.in>
2023-06-07 19:45:18 +0530

    Practicing DP

---
+++ cutRod.py
@@ -0,0 +1,10 @@
+def cutRod(p, n):
+    if n == 0:
+        return 0
+    q = -1
+    for i in range(1, n + 1):
+        q = max(q, p[i] + cutRod(p, n - i))
+    return q
+
+p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
+print(cutRod(p, 6))
