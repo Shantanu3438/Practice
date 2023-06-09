a72bfc19be3a2da15a439632f5827c2ecec22cc3
Shantanu <shantanuanand@live.in>
2023-06-09 19:29:00 +0530

    bottom-up DP

--- cutRod.py
+++ cutRod.py
@@ -1,10 +1,18 @@
-def cutRod(p, n):
+def cutRod(p, n, r):
+    if r[n] >= 0:
+        return r[n]
     if n == 0:
-        return 0
-    q = -1
+        q = 0
+    else:
+        q = - 1
     for i in range(1, n + 1):
-        q = max(q, p[i] + cutRod(p, n - i))
+        pass
+        q = max(q, p[i] + cutRod(p, n - i, r))
+        r[n] = q
     return q
 
+r= [-1, -1, -1, -1, -1, -1, -1]
 p = [0, 1, 5, 8, 9, 10, 17, 17, 20, 24, 30]
-print(cutRod(p, 6))
+print(cutRod(p, 4, r))
+for i in range(1, 6):
+    print(r[i])
