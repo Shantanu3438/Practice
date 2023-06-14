b8526259541986c2de1c73877c21387f5f94e3fb
Shantanu <shantanuanand@live.in>
2023-06-14 17:45:12 +0530

    Downloads are now queuing

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -6,27 +6,31 @@ from qbittorrent import Client
 qb = Client("http://127.0.0.1:8080/")
 qb.login("admin", "adminadmin")
 
-site = requests.get("https://www.imdb.com/chart/top/?ref_=nv_mv_250")
+site = requests.get("https://www.imdb.com/chart/toptv/?ref_=nv_tvv_250")
+print(site.text)
 siteSoup = bs(site.text, 'html.parser')
-tags = siteSoup.find_all('a')
 releaseDate = siteSoup.find_all('span', class_ = 'secondaryInfo')
 movieTitle = siteSoup.select('td a', class_ = 'titleColumn')
-movieList = open('movieList.txt', 'w')
+movieList = open('tv.txt', 'w')
 for movie in movieTitle:
+    #print(movie.text)
     movieList.write(movie.text)
-
+    
 movieList.close()
 
-movieFile = open('movieList.txt')
+movieFile = open('tv.txt')
 movieList = movieFile.read().splitlines()
 
 for movie in movieList:
+    if movie == ' ':
+        print('empty')
+        continue
     movieLink = "https://pbays.top/search/" + movie + "/1/99/0"
     print(movieLink)
     movieSite = requests.get(movieLink)
     movieSoup = bs(movieSite.text, 'html.parser')
-    movieMagnetLink = movieSoup.select("a[title = 'Download this torrent using magnet']")
-    print(movieMagnetLink)
-    #magnetLink = movieMagnetLink[1].attrs['href']
+    movieMagnetLink = movieSoup.find("a", {'title' : 'Download this torrent using magnet'})
+    print(movieMagnetLink.attrs['href'])
+    magnetLink = movieMagnetLink.attrs['href']
     #print(magnetLink)
-    #qb.download_from_link(magnetLink)
+    qb.download_from_link(magnetLink)
